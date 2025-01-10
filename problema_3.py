f75df880c42a8fea31e061f95ccfcf6e8e2f9543
Victória Barbosa <victoria.barbosab26@gmail.com>
2025-01-10 16:27:16 -0300

    Update problema_3.py

--- problema_3.py
+++ problema_3.py
@@ -3,12 +3,12 @@ import xml.etree.ElementTree as ET
 
 # Função para carregar os dados do JSON
 def carregar_dados_json():
-    with open('dados.json', 'r') as file:  # Nome do arquivo JSON na mesma pasta
+    with open('dados.json', 'r') as file: 
         return json.load(file)
 
 # Função para carregar os dados do XML
 def carregar_dados_xml():
-    tree = ET.parse('dados(2).xml')  # Nome do arquivo XML na mesma pasta
+    tree = ET.parse('dados(2).xml')
     root = tree.getroot()
     return [{"dia": int(row.find("dia").text), "valor": float(row.find("valor").text)} for row in root.findall("row")]
 
@@ -29,18 +29,18 @@ def calcular_metricas(faturamento_diario):
 
     return menor_faturamento, maior_faturamento, media_faturamento, dias_acima_da_media
 
-# Carregar os dados (JSON ou XML)
+# Carrega os dados (JSON ou XML)
 faturamento_diario_json = carregar_dados_json()
 faturamento_diario_xml = carregar_dados_xml()
 
-# Calcular as métricas para os dados JSON
+# Calcula as métricas para os dados JSON
 menor_faturamento_json, maior_faturamento_json, media_faturamento_json, dias_acima_da_media_json = calcular_metricas(faturamento_diario_json)
 print(f"Resultado para dados.json:")
 print(f"Menor faturamento: R${menor_faturamento_json:.2f}")
 print(f"Maior faturamento: R${maior_faturamento_json:.2f}")
 print(f"Dias acima da média mensal: {dias_acima_da_media_json} dias")
 
-# Calcular as métricas para os dados XML
+# Calcula as métricas para os dados XML
 menor_faturamento_xml, maior_faturamento_xml, media_faturamento_xml, dias_acima_da_media_xml = calcular_metricas(faturamento_diario_xml)
 print(f"\nResultado para dados(2).xml:")
 print(f"Menor faturamento: R${menor_faturamento_xml:.2f}")
